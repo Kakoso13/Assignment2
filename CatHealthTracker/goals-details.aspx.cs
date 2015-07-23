6150ecd401d15d27089ca24f5a5a8fd0cdae9c42
HRubyta <200183528@student.georgianc.on.ca>
2015-07-24 03:14:21 +0700

    load gridview data to goals

--- CatHealthTracker/goals-details.aspx.cs
+++ CatHealthTracker/goals-details.aspx.cs
@@ -1,17 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//reference to the db model
+using CatHealthTracker.Models;
+
 namespace CatHealthTracker
 {
-    public partial class goals_details : System.Web.UI.Page
+    public partial class goals : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetGoals();
+            }
+        }
 
+        protected void GetGoals()
+        {
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                var Goals = from g in db.Goallogs
+                               select g;
+
+                grdGoals.DataSource = Goals.ToList();
+                grdGoals.DataBind();
+            }
         }
     }
 }
