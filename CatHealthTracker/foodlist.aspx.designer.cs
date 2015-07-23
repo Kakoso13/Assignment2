971082fc1a54e704d5537985db2b5fd9fa0ae163
HRubyta <200183528@student.georgianc.on.ca>
2015-07-24 03:10:36 +0700

    Update foodlist designer page

--- CatHealthTracker/foodlist.aspx.designer.cs
+++ CatHealthTracker/foodlist.aspx.designer.cs
@@ -10,51 +10,15 @@
 namespace CatHealthTracker {
     
     
-    public partial class foodlog_details {
+    public partial class foodlist {
         
         /// <summary>
-        /// txtFoodname control.
+        /// grdFoodlist control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtFoodname;
-        
-        /// <summary>
-        /// txtCalCount control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtCalCount;
-        
-        /// <summary>
-        /// ddlFoodtype control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlFoodtype;
-        
-        /// <summary>
-        /// ddlDay control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlDay;
-        
-        /// <summary>
-        /// btnSave control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSave;
+        protected global::System.Web.UI.WebControls.GridView grdFoodlist;
     }
 }
