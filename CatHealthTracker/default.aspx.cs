ab52e39faec564dfbea916d71c7da2d0d8b4dbe8
HRubyta <200183528@student.georgianc.on.ca>
2015-07-24 00:03:44 +0700

    Add calculation function
    calculate the feline calorie calculator on submit button

--- CatHealthTracker/default.aspx.cs
+++ CatHealthTracker/default.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +12,22 @@ namespace CatHealthTracker
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //calculate the weight from pound to kg
+            Double weight = Math.Round(Convert.ToDouble(txtWeight.Text) / 2.2);
+
+            //display the weight in kg
+            txtMass.Text = weight.ToString();
+
+            //calculate the calories
+            Double cal = (Math.Round(Convert.ToDouble(txtMass.Text)) * 30) + 70;
+            Double finalCal = cal * 0.8;
+
+            //display the total calories in a day
+            txtCalorie.Text = finalCal.ToString();
         }
     }
 }
