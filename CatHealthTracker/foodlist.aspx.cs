fab60276b8236f7736db7224001acbf2ae4000c3
HRubyta <200183528@student.georgianc.on.ca>
2015-07-24 03:09:12 +0700

    load gridview data to foodlist

--- CatHealthTracker/foodlist.aspx.cs
+++ CatHealthTracker/foodlist.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
+//reference to the db model
+using CatHealthTracker.Models;
 
 namespace CatHealthTracker
 {
@@ -11,7 +14,23 @@ namespace CatHealthTracker
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetGoals();
+            }
+        }
 
+        protected void GetGoals()
+        {
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                var Foodlist = from f in db.Foodlists
+                               select f;
+
+                grdFoodlist.DataSource = Foodlist.ToList();
+                grdFoodlist.DataBind();
+
+            }
         }
     }
 }
